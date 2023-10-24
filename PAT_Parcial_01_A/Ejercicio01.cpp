d0851a3e0676bedeed91afd5ec859b6ea7215f9f
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-10-24 07:49:04 -0600

    Ejercicio 1

--- PAT_Parcial_01_A/Ejercicio01.cpp
+++ PAT_Parcial_01_A/Ejercicio01.cpp
@@ -2,5 +2,35 @@
 
 Node<char>* Ejercicio01::remove(Node<char>* head, char value)
 {
-    return nullptr;
+	Node<char>* prev = nullptr;
+	Node<char>* temp = head;
+
+	if (head == nullptr)
+		return nullptr;
+
+	while (temp != nullptr)
+	{
+		if (temp->value == value)
+		{
+			if (prev == nullptr)
+			{
+				head = temp->next;
+				delete temp; 
+				temp = head;
+			}
+			else
+			{
+				prev->next = temp->next;
+				delete temp;
+				temp = prev->next;
+			}
+		}
+		else
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+	}
+	return head;
 }
+
--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -4,3 +4,35 @@ int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
     return 0;
 }
+
+class Cola
+{
+private:
+    struct Node
+    {
+        char valor;
+        Node* next = nullptr;
+        Node(char valor) : valor(valor) {}
+    };
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+public:
+    void enque(char valor)
+    {
+        if (head == nullptr)
+        {
+            head = tail = new Node(valor);
+        }
+        else
+        {
+            tail->next = new Node(valor);
+            tail = tail->next;
+        }
+    }
+
+    void deque()
+    {
+
+    }
+};
